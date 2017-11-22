ba524a3c33972e580eaa894b374ceb0e500277e9
Razeral <ng.shangru@gmail.com>
2017-11-22 16:15:02 +0800

    1

--- TestSync/index.js
+++ TestSync/index.js
@@ -1,3 +1,21 @@
+// Cosmos DB
+/*
+var documentDbOptions = {
+    host: process.env['CosmosDB_HOST'],
+    masterKey: process.env['CosmosDB_MASTER_KEY'],
+    database: process.env['CosmosDB_DATABASE'],
+    collection: process.env['CosmosDB_COLLECTION']
+};
+var docDbClient = new botbuilder_azure.DocumentDbClient(documentDbOptions);
+var cosmosStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, docDbClient);
+//bot.set('storage', cosmosStorage); // Why doesnt this work???
+*/
+
+var documentClient = require("documentdb").DocumentClient;
+//var config = require("./config");
+var url = require('url');
+var client = new documentClient(process.env['CosmosDB_HOST'],process.env['CosmosDB_MASTER_KEY']);
+
 module.exports = function(context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
@@ -5,7 +23,7 @@ module.exports = function(context, req) {
     {
         context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "Hello " + (req.query.name || req.body.name)
+        body: "Hello " + (req.query.name || req.body.name) + " " + client,
         };
     }
     else
@@ -13,7 +31,7 @@ module.exports = function(context, req) {
         context.res = {
         status: 400,
             body: "Please pass a name on the query string or in the request body"
-        };
+        },
     }
     context.done();
 };
